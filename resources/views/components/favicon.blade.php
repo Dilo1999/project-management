3d00dc754542df4be5ac3e0ee721c2fdcb97e799
Dilo1999 <nimeshdilshan440@gmail.com>
2026-03-20 10:31:21 +0530

    Add favicon inclusion to multiple views for consistent branding across the application.

---
+++ resources/views/components/favicon.blade.php
@@ -0,0 +1,3 @@
+{{-- Site favicon (public/images/favicon/fav.png) --}}
+<link rel="icon" type="image/png" href="{{ asset('images/favicon/fav.png') }}" sizes="32x32">
+<link rel="apple-touch-icon" href="{{ asset('images/favicon/fav.png') }}">
